371620a18389f091a9696173bde2acecff28ff6a
Juliana <j.amaralwork@gmail.com>
2023-06-06 11:22:43 +0300

    feedback

--- start.bat
+++ start.bat
@@ -1,10 +1,10 @@
 @echo off
 
 
-python "C:\Users\den-s\OneDrive\Desktop\HackathonFinal\Parser\seleniumParser.py"
+python "C:\Users\JuJu\Desktop\DI-Bootcamp\test\Hackathon\Denis\HackhatonParser\Parser\seleniumParser.py"
 
-python "C:\Users\den-s\OneDrive\Desktop\HackathonFinal\Parser\beautifulsoup.py"
+python "C:\Users\JuJu\Desktop\DI-Bootcamp\test\Hackathon\Denis\HackhatonParser\Parser\beautifulsoup.py"
 
-python "C:\Users\den-s\OneDrive\Desktop\HackathonFinal\DataBase\main.py"
+python "C:\Users\JuJu\Desktop\DI-Bootcamp\test\Hackathon\Denis\HackhatonParser\DataBase\main.py"
 
-python "C:\Users\den-s\OneDrive\Desktop\HackathonFinal\TelegramBot\testbot.py"
+python "C:\Users\JuJu\Desktop\DI-Bootcamp\test\Hackathon\Denis\HackhatonParser\TelegramBot\testbot.py"
